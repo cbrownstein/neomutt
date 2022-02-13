fbfa7582450de1edb24362e1fad45f3514f21497
Richard Russon <rich@flatcap.org>
2022-02-13 01:13:46 +0000

    window: add function dispatcher
    Let a Window hold a function dispatcher.
    This will allows Dialogs to delegate functions without having to know
    which dispatchers there are.

---
+++ gui/global.c
@@ -0,0 +1,107 @@
+/**
+ * @file
+ * Global functions
+ *
+ * @authors
+ * Copyright (C) 2022 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @page gui_global Global functions
+ *
+ * Global functions
+ */
+
+#include "config.h"
+#include <stddef.h>
+#include "mutt/lib.h"
+#include "core/lib.h"
+#include "gui/lib.h"
+#include "global.h"
+#include "lib.h"
+#include "index/lib.h"
+#include "muttlib.h"
+#include "opcodes.h"
+
+/**
+ * traverse_tree - Traverse a tree of Windows to find function to handle an operation
+ * @param win    Window to start at
+ * @param ignore Child Window to ignore
+ * @param op     Operation to perform, e.g. OP_VERSION
+ * @retval num #IndexRetval, e.g. #IR_SUCCESS
+ *
+ * Descend through a tree of Windows.  If a Window has a function dispatcher, run it.
+ * If it can handle the operation, then finish.
+ *
+ * Non-visible windows will be ignored.
+ */
+static int traverse_tree(struct MuttWindow *win, struct MuttWindow *ignore, int op)
+{
+  if (!win || !win->state.visible)
+    return IR_UNKNOWN;
+
+  int rc;
+  if (win->function)
+  {
+    rc = win->function(win, op);
+    if (rc != IR_UNKNOWN)
+      return rc;
+  }
+
+  struct MuttWindow *np = NULL;
+  TAILQ_FOREACH(np, &win->children, entries)
+  {
+    if (np == ignore)
+      continue;
+
+    rc = traverse_tree(np, NULL, op);
+    if (rc != IR_UNKNOWN)
+      return rc;
+  }
+
+  return IR_UNKNOWN;
+}
+
+/**
+ * window_dispatch_function - Search for a handler for an operation
+ * @param win Window to start at
+ * @param op  Operation to perform, e.g. OP_VERSION
+ * @retval num #IndexRetval, e.g. #IR_SUCCESS
+ *
+ * Search through a tree of Windows looking for one with a function dispatcher
+ * that can handle op.
+ *
+ * Start at the given Window, then search it's children.
+ * If that fails climb the tree looking wider for matches.
+ */
+int window_dispatch_function(struct MuttWindow *win, int op)
+{
+  if (!mutt_window_is_visible(win))
+    return IR_UNKNOWN;
+
+  struct MuttWindow *ignore = NULL;
+
+  int rc;
+  for (; win; ignore = win, win = win->parent)
+  {
+    rc = traverse_tree(win, ignore, op);
+    if (rc != IR_UNKNOWN)
+      return rc;
+  }
+
+  return IR_UNKNOWN;
+}
